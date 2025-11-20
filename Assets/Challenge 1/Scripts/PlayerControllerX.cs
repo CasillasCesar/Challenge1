75e1e7f8eb980c8b4f37dcf7189dcd70252c2a29
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2025-11-20 14:43:34 -0600

    Slow the plane down to a manageable speed.

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    private float speed = 2;
     public float rotationSpeed;
     public float verticalInput;
 
